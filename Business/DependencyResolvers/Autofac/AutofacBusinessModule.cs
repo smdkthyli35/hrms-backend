3f3beadfed17fffb53c662a0ef2c9f7d355dce3a
zeljkosamed <smdkthyli@gmail.com>
2022-02-26 23:27:47 +0300

    BusinessRules eklendi.

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Repositories;
 using System;
@@ -68,6 +71,15 @@ namespace Business.DependencyResolvers.Autofac
 
             builder.RegisterType<WorkingTypeManager>().As<IWorkingTypeService>().SingleInstance();
             builder.RegisterType<EfWorkingTypeDal>().As<IWorkingTypeDal>().SingleInstance();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
+
         }
     }
 }
